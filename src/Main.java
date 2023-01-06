6cf4b56f3515e82566d9932c45ad9b1fd03b45dd
양창욱(Changwook Yang)/Biz플랫폼개발팀/SKP <ycw92325@gmail.com>
2023-01-06 22:01:34 +0900

    최단 거리의 리프노드 찾기(BFS)

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Node {
     int data;
     Node lt, rt;
@@ -18,6 +21,30 @@ public class Main {
             return Math.min(DFS(L+1, root.lt), DFS(L+1, root.rt));
     }
 
+    public static int BFS(Node root) {
+        int L = 0;
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+                if (node.lt == null && node.rt == null) {
+                    return L;
+                } else {
+                    if(node.lt != null)
+                        queue.offer(node.lt);
+                    if(node.rt != null)
+                        queue.offer(node.rt);
+                }
+            }
+            L++;
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         Main tree = new Main();
         tree.root = new Node(1);
@@ -26,7 +53,9 @@ public class Main {
         tree.root.lt.lt = new Node(4);
         tree.root.lt.rt = new Node(5);
         int result = DFS(0, tree.root);
+        int result2 = BFS(tree.root);
         System.out.println(result);
+        System.out.println(result2);
     }
 
 }
