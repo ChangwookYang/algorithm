768bbce8f067dbf4f4a4bf9ed5d0ed6db21f7ad9
양창욱(Changwook Yang)/Biz플랫폼개발팀/SKP <ycw92325@gmail.com>
2023-01-06 21:50:29 +0900

    최단 거리의 리프노드 찾기(DFS)

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,11 @@ class Node {
 public class Main {
     Node root;
 
-    public int DFS(int L, Node root) {
-
+    public static int DFS(int L, Node root) {
+        if (root.lt == null && root.rt == null) {
+            return L;
+        } else
+            return Math.min(DFS(L+1, root.lt), DFS(L+1, root.rt));
     }
 
     public static void main(String[] args) {
@@ -22,6 +25,8 @@ public class Main {
         tree.root.rt = new Node(3);
         tree.root.lt.lt = new Node(4);
         tree.root.lt.rt = new Node(5);
+        int result = DFS(0, tree.root);
+        System.out.println(result);
     }
 
 }
