ca2eed1dd26a5d4d96efd5dea92da5beaf75ad5a
양창욱(Changwook Yang)/Biz플랫폼개발팀/SKP <ycw92325@gmail.com>
2023-01-07 18:35:44 +0900

    그래프 최단거리 (BFS)

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,18 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
 
-
+    static int n, m;
     static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-    static int[] check;
-    static int answer;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
+        n = scanner.nextInt();
+        m = scanner.nextInt();
 
-        check = new int[n + 1];
-
-        answer = 0;
-        check[1] = 1;
         for (int i = 0; i <= n; i++) {
             list.add(new ArrayList<>());
         }
@@ -27,23 +24,67 @@ public class Main {
             list.get(a).add(b);
         }
 
-        DFS(1, n);
-        System.out.println(answer);
+        DFS_TREE(1);
+        DFS_DIST(1);
     }
 
-    private static void DFS(int node, int lastNode) {
-        if (node == lastNode) {
-            answer++;
-        } else {
-            for (int val : list.get(node)) {
-                if (check[val] == 0) {
-                    check[node] = 1;
-                    DFS(val, lastNode);
-                    check[node] = 0;
+    private static void DFS_DIST(int root) {
+        int[] check = new int[n + 1];
+        int[] dist = new int[n + 1];
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(root);
+        check[1] = 1;
+        dist[1] = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Integer node = queue.poll();
+                for (int neigbhor : list.get(node)) {
+                    if (check[neigbhor] == 0) {
+                        queue.offer(neigbhor);
+                        check[neigbhor] = 1;
+                        dist[neigbhor] = dist[node] + 1;
+                    }
                 }
             }
         }
 
+        for (int i = 2; i < n + 1; i++) {
+            System.out.println(i + " : " + dist[i]);
+        }
     }
+
+
+
+    private static void DFS_TREE(int root) {
+        int[] check = new int[n + 1];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(root);
+
+        check[1] = 1;
+        int distance = 1;
+        int[] result = new int[n + 1];
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Integer cur = queue.poll();
+                for (int val : list.get(cur)) {
+                    if (check[val] == 0) {
+                        queue.offer(val);
+                        check[val] = 1;
+                        result[val] = distance;
+                    }
+                }
+            }
+            distance++;
+        }
+
+        for (int i = 2; i < n + 1; i++) {
+            System.out.println(i + " : " + result[i]);
+        }
+    }
+
 
 }
