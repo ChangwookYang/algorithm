b53438f8da4a037d6433156e16e804c4aeb442bb
양창욱 <ycw92325@gmail.com>
2023-02-26 23:29:46 +0900

    프로그래머스 기본문제 풀이

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,27 @@
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
-//        String[] keymap = new String[]{"ABACD", "BCEFD"};
-//        String[] targets = new String[]{"ABCD", "AABB"};
-        String[] keymap = new String[]{"AGB", "BSSS"};
-        String[] targets = new String[]{"AGZ", "BSSS"};
-        int[] solution = solution(keymap, targets);
-        for (int sol : solution) {
-            System.out.println(sol);
-        }
+        int[][] dots = new int[][]{{1, 4}, {9, 2}, {3, 8}, {11, 6}};
+
+        int answer = solution(dots);
+        System.out.println(answer);
     }
 
-    public static int[] solution(String[] keymap, String[] targets) {
-        ArrayList<Integer> list = new ArrayList<>();
+    public static int solution(int[][] dots) {
+        int answer = 0;
 
-        for (String target : targets) {
-            int cnt = 0;
-            // target 기준 for문
-            boolean isSelected = true;
-            for (int i = 0; i < target.length(); i++) {
-                isSelected = false;
-                // key 값 기준
-                for (int keyIndex = 0; keyIndex < 100 && !isSelected; keyIndex++) {
-                    for (String key : keymap) {
-                        if (keyIndex >= key.length()) {
-                            // 초과할 경우 무시
-                            continue;
-                        } else {
-                            if (key.charAt(keyIndex) == target.charAt(i)) {
-                                isSelected = true;
-                                cnt += (keyIndex + 1);
-                                break;
-                            }
-                        }
-                    }
-                }
-                if(!isSelected) break;
+        Set<Double> set = new HashSet<>();
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = i + 1; j < 4; j++) {
+                Double lin = (dot)
+                set.add()
             }
-            if(!isSelected || cnt == 0) list.add(-1);
-            else list.add(cnt);
         }
 
-        return list.stream().mapToInt(i -> i).toArray();
+        return answer;
     }
 }
