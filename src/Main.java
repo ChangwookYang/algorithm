e378a45799f4d724312fbe8bfa32c398d07cb3eb
양창욱 <ycw92325@gmail.com>
2023-06-17 20:43:24 +0900

    merge node list method 1(brute force)

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,68 @@ public class Main {
 
     public static void main(String[] args) {
 
+        ListNode[] node = new ListNode[] {
+                new ListNode(1, new ListNode(4, new ListNode(5))),
+                new ListNode(1, new ListNode(3, new ListNode(4))),
+                new ListNode(2, new ListNode(6))
+        };
+
+
+        ListNode listNode = mergeKLists2(node);
+        while (listNode != null) {
+            System.out.println(listNode.val + " ");
+            listNode = listNode.next;
+        }
+    }
+    public static ListNode mergeKLists2(ListNode[] lists) {
+        int interval = 1;
+
+        while (interval < lists.length) {
+            for (int i = 0; i < lists.length - interval; i += 2 * interval) {
+                ListNode listNode1 = lists[i];
+                ListNode listNode2 = null;
+                if(i+interval < lists.length) {
+                    listNode2 = lists[i + interval];
+                }
+                lists[i] = mergeTwoList(listNode1, listNode2);
+            }
+            interval = interval * 2;
+        }
+
+        return lists[0];
     }
 
-    public ListNode mergeKLists(ListNode[] lists) {
+    private static ListNode mergeTwoList(ListNode list1, ListNode list2) {
+        ListNode merged = new ListNode(0);
+        ListNode returnNode = merged;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                merged.next = new ListNode(list1.val);
+                list1 = list1.next;
+            } else {
+                merged.next = new ListNode(list2.val);
+                list2 = list2.next;
+            }
+            merged = merged.next;
+        }
+
+        while (list1 != null) {
+            merged.next = new ListNode(list1.val);
+            list1 = list1.next;
+            merged = merged.next;
+        }
+
+        while (list2 != null) {
+            merged.next = new ListNode(list2.val);
+            list2 = list2.next;
+            merged = merged.next;
+        }
+
+        return returnNode.next;
+    }
+
+
+    public static ListNode mergeKLists1(ListNode[] lists) {
         List<Integer> nodeList = new ArrayList<>();
 
         for (ListNode node : lists) {
@@ -31,7 +90,7 @@ public class Main {
         return answer.next;
     }
 
-    public class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
 
