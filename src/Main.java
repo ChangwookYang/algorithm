8ef7a60ae828bcc9d9960719beb98224d6253ee6
양창욱 <ycw92325@gmail.com>
2023-06-17 20:18:11 +0900

    merge node list method 1(brute force)

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,50 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
-    static int[] plus = new int[]{-1, 0, 1};
-
     public static void main(String[] args) {
-        int result = maxLevelSum(new TreeNode(1));
+
     }
 
-    public static int maxLevelSum(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        int level = 1;
-        int max = Integer.MIN_VALUE;
-        int maxLevel = 0;
-        while (!queue.isEmpty()) {
-            int length = queue.size();
-            int sum = 0;
-            for (int i = 0; i < length; i++) {
-                TreeNode node = queue.poll();
-                if (node != null) {
-                    sum += node.val;
-                    if (node.left != null) {
-                        queue.offer(node.left);
-                    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        List<Integer> nodeList = new ArrayList<>();
 
-                    if (node.right != null) {
-                        queue.offer(node.right);
-                    }
-                }
+        for (ListNode node : lists) {
+            while (node != null) {
+                nodeList.add(node.val);
+                node = node.next;
             }
-            if (sum > max) {
-                max = sum;
-                maxLevel = level;
-            }
-            level++;
         }
 
-        return maxLevel;
+        Collections.sort(nodeList);
+
+
+        ListNode dummy = new ListNode(0);
+        ListNode answer = dummy;
+        for (Integer val : nodeList) {
+            dummy.next = new ListNode(val);
+            dummy = dummy.next;
+        }
+
+        return answer.next;
     }
 
-    public static class TreeNode {
+    public class ListNode {
         int val;
-        TreeNode left;
-        TreeNode right;
+        ListNode next;
 
-        TreeNode() {
+        ListNode() {
         }
 
-        TreeNode(int val) {
+        ListNode(int val) {
             this.val = val;
         }
 
-        TreeNode(int val, TreeNode left, TreeNode right) {
+        ListNode(int val, ListNode next) {
             this.val = val;
-            this.left = left;
-            this.right = right;
+            this.next = next;
         }
     }
 }
